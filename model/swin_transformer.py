781550a9c8eddac9b420bda64e8baa56b1065ec4
jhtobigs <jhtobigs@gmail.com>
2022-04-19 18:20:22 +0000

    Add window partition & reverse function

--- model/swin_transformer.py
+++ model/swin_transformer.py
@@ -3,6 +3,39 @@ from tensorflow.keras.layers import Layer, Conv2D, Dense, LayerNormalization, Fl
 from transformer.transformer import MultiHeadAttention
 
 
+def window_partition(x, window_size=7):
+    """
+    Args:
+        x: (B, H, W, C)
+        window_size (int): window size
+
+    Returns:
+        windows: (num_windows*B, window_size, window_size, C)
+    """
+    B, H, W, C = x.shape
+    x = tf.reshape(x, (B, H // window_size, window_size, W // window_size, window_size, C))
+    windows = tf.reshape(tf.transpose(x, (0, 1, 3, 2, 4, 5)), (-1, window_size, window_size, C))
+
+    return windows
+
+
+def window_reverse(windows, window_size, H, W):
+    """
+    Args:
+        windows: (num_windows*B, window_size, window_size, C)
+        window_size (int): Window size
+        H (int): Height of image
+        W (int): Width of image
+
+    Returns:
+        x: (B, H, W, C)
+    """
+    B = int(windows.shape[0] / (H * W / window_size / window_size))
+    x = tf.reshape(windows, (B, H // window_size, W // window_size, window_size, window_size, -1))
+    x = tf.reshape(tf.transpose(x, (0, 1, 3, 2, 4, 5)), (B, H, W, -1))
+    return x
+
+
 class PatchEncoder(Layer):
     def __init__(self, image_size, dims, patch_size=4):
         super().__init__()
