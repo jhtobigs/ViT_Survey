e7cd9d662c6c15a94a6800f84eeafe736c559641
jhtobigs <jhtobigs@gmail.com>
2022-05-08 16:42:39 +0000

    feat : Add BEiTPatchEncoder

--- model/beit.py
+++ model/beit.py
@@ -1,36 +1,37 @@
+from requests import patch
 import tensorflow as tf
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Layer, Dense, Dropout, Embedding, LayerNormalization
+from tensorflow.keras.layers import Layer, Dense, Conv2D, Dropout, Embedding, LayerNormalization
 from model.transformer import EncoderBlock
-from model.vit import PatchPartition, PatchEmbedding, PositionalEmbedding
-
-
-class BlockwiseMasking(Layer):
-    def __init__(self):
-        super().__init__()
-
-    def call(self, x):
-        return x
+from model.vit import PositionalEmbedding
+from model.utils.mask_generator import MaskingGenerator
 
 
 class BEiTPatchEncoder(Layer):
     def __init__(self, patch_size: int, num_patches: int, dims: int):
         super().__init__()
-        self.patch_masking = BlockwiseMasking()
-        self.patch_split = PatchPartition(patch_size)
-        self.patch_embedding = PatchEmbedding(dims)
+        self.dims = dims
+        self.cls_token = tf.Variable(tf.random.truncated_normal(shape=(1, 1, dims)), trainable=True)
+        self.mask_token = tf.Variable(tf.random.truncated_normal(shape=(1, 1, dims)), trainable=True)
+        self.patch_masking = MaskingGenerator(patch_size, num_masking_patches=int(num_patches * 0.4))
+        self.patch_embedding = Conv2D(filters=dims, kernel_size=patch_size, strides=patch_size)
         self.position_embedding = PositionalEmbedding(num_patches, dims)
 
-    def _append_class_token(self, patches):
-        class_token = tf.zeros_like(patches[:, :1, :])
-        patches = tf.concat([class_token, patches], axis=1)
+    def _blockwise_masking(self, patches, batch_size: int):
+        mask_patch = self.patch_masking(batch_size)[..., tf.newaxis]
+        patches = patches * (1 - mask_patch) + (self.mask_token * mask_patch)
+        return patches
+
+    def _process_token(self, patches):
+        B = tf.shape(patches)[0]
+        patches = self._blockwise_masking(patches, B)
+        patches = tf.reshape(patches(B, -1, self.dims))
+        patches = tf.concat([self.cls_token, patches], axis=1)
         return patches
 
     def call(self, x):
-        mask = self.patch_masking(x)
-        x = self.patch_split(x)
         x = self.patch_embedding(x)
-        x = self._append_class_token(x)
+        x = self._process_token(x)
         x += self.position_embedding(x)
         return x
 
